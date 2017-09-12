eba17128e11fa8f91c81a453a3ddaeef78efd621
Shawn Toubeau <shawntoubeau@gmail.com>
2017-09-11 22:29:39 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,27 +22,29 @@ jQuery(document).ready(function($) {
   $(window).stellar();
   
   
-  $("#nav ul li a[href^='#']").on('click', function(e) {
+   $('body').scrollspy({target: ".navbar", offset: 50});   
 
-   // prevent default anchor click behavior
-   e.preventDefault();
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-   // store hash
-   var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 1000, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
-  
-  
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });  
 });
 
 //OPENS THE MODAL TO DISPLAY SNAPCODE
