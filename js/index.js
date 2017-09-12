4f403cac953d04ac818556d17e76050f7f96f6b6
Shawn Toubeau <shawntoubeau@gmail.com>
2017-09-11 21:35:43 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,29 +22,25 @@ jQuery(document).ready(function($) {
   $(window).stellar();
   
   
-  $('body').scrollspy({target: ".navbar", offset: 50});   
+  $("#nav ul li a[href^='#']").on('click', function(e) {
 
-  // Add smooth scrolling on all links inside the navbar
-  $("#myNavbar a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+   // prevent default anchor click behavior
+   e.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+   // store hash
+   var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 1000, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
   
   
 });
